1c7f06122c2ed47f4b0680ccdac9763a5f708c8e
Saidnurmuhammadulloxon <nasimkhonovs@gmail.com>
2025-09-04 13:06:10 +0500

    z

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,23 +1,33 @@
-import {  useDispatch } from 'react-redux'
-import { increment, decrement , incrementByAmount} from '../redux/counter-slice/counterSlice'
-import type { AppDispatch } from '../redux/store'
+import { useDispatch } from "react-redux";
+import {
+  increment,
+  decrement,
+  incrementByAmount,
+} from "../redux/counter-slice/counterSlice";
+import type { AppDispatch } from "../redux/store";
 
-
-const Button = ({titile}:{titile:string}) => {
-
-    const dispatch = useDispatch<AppDispatch>()
+const Button = ({ titile }: { titile: string }) => {
+  const dispatch = useDispatch<AppDispatch>();
 
   return (
-
     <div>
-        { titile }  <br /> <br />
-        
-        <button className='bg-red-500' onClick={()=> dispatch(increment())}>increment</button> <br />  <br />
-        <button className='bg-red-500'  onClick={()=> dispatch(decrement())}>decrement</button>  <br />  <br />
-        <button className='bg-red-500'  onClick={()=> dispatch(incrementByAmount(10))}>IncrementBy amount</button>
+      {titile} <br /> <br />
+      <button className="bg-red-500" onClick={() => dispatch(increment())}>
+        increment
+      </button>{" "}
+      <br /> <br />
+      <button className="bg-red-500" onClick={() => dispatch(decrement())}>
+        decrement
+      </button>{" "}
+      <br /> <br />
+      <button
+        className="bg-red-500"
+        onClick={() => dispatch(incrementByAmount(10))}
+      >
+        IncrementBy amount
+      </button>
     </div>
-  )
-}
+  );
+};
 
-
-export default Button
+export default Button;
